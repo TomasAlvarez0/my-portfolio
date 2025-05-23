884156b76ca5397797af13b6fa18d7866b78128d
Tomas Alvarez <martin.08@live.com.ar>
2025-05-22 23:32:02 -0300

    Add header and main components, implement smooth scrolling and custom fonts

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let y;
+
+    let tabs = [
+        {name: "Projects", link: "#projects"},
+        {name: "About me", link: "#about"},
+        {name: "Contact", link: "#contact"}
+    ]
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid'+
+(y > 0 ? 'py-4 bg-slate-900 border-violet-900' : 'py-6 bg-transparent border-transparent')}>
+    <h1>
+        <b>Tomas</b> Alvarez
+    </h1>
+    <div class="">
+        {#each tabs as tab, index}
+        <a href={tab.link}>
+            <p>{tab.name}</p>
+        </a>
+            
+        {/each}
+    </div>
+</header>
