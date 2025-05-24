700c9638b970253615c5077761fba8d064f44409
Tomás <martin.08.mta@gmail.com>
2025-05-23 21:05:13 -0300

    Agregar componentes de pie de página y paso, actualizar el encabezado y el contenido principal con secciones sobre proyectos y beneficios, además de incluir una imagen de perfil.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,6 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 items-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        © 2023 Samuel Oak
+    </p>
+    <p class="text-slate-600">Created with SvelteKit and TailwindCSS 🔥</p>
+</footer>
---
+++ src/components/Step.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let step;
+</script>
+
+<div class="flex flex-col gap-4 p-6 rounded-lg border border-solid border-slate-700 text-center group cursor-pointer hover:border-violet-700 duration-200">
+    <div class="bg-slate-950 text-white rounded-full mx-auto grid place-items-center w-12 h-12 text-2xl group-hover:bg-violet-700 duration-200">
+        <i class={step.icon}></i>
+    </div>
+    <h4 class="text-xl md:text-2xl">{step.name}</h4>
+    <p class="text-base sm:text-lg opacity-70 group-hover:opacity-100 duration-200">
+        <slot />
+    </p>
+</div>
